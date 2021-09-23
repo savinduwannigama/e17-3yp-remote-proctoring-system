10c65914e2495c32352f99018fd5a0ec8cebb984
isuridevindi <e17058@eng.pdn.ac.lk>
2021-09-23 14:57:03 +0530

    Fix Admin Upload button

--- Proctor_webapp/src/components/Adminbtn.js
+++ Proctor_webapp/src/components/Adminbtn.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 import Stack from '@mui/material/Stack';
+import axios from 'axios';
 
 const Input = styled('input')({
   display: 'none',
@@ -20,24 +21,77 @@ const theme = createTheme({
     },
 });
 
-export default function Adminbtn(props) {
-  return (
-    <Stack direction="row" alignItems="center" spacing={2}>
-      <label htmlFor="contained-button-file">
-        <Input accept="image/*" id="contained-button-file" multiple type="file" />
-        <ThemeProvider theme={theme}>
-        <Button color="neutral" variant="contained" component="span" size="large">
-            {props.btnname}
-          <IconButton style={{ color:'white' }} aria-label="upload picture" component="span">
-          <UploadFileIcon fontSize="large"/>
-          </IconButton>
-        </Button>
-        </ThemeProvider>
-      </label>
-      <label htmlFor="icon-button-file">
-        <Input accept="image/*" id="icon-button-file" type="file" />
+export default class Adminbtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.upload= "Upload ";
+    this.id = props.value;
+    this.btnname = props.btnname;
+    this.state = {
+      selectedFile: undefined,
+      FileName: "No File Selected",
+      currentFile: undefined,
+      progress: 0,
+      message: "",
+      isError: false,
+      fileInfos: [],
+    };
+  }
+  
+  fileSelectedHandler = event => {
+    
+    console.log(event.target.files);
+    this.setState({
+      selectedFile: event.target.files[0],
+      FileName: this.upload+event.target.files[0].name
+    })
+  }
+
+  fileUploadHandler = ()=>{
+    const fd = new FormData();
+    fd.append(this.value, this.state.selectedFile,this.state.selectedFile.name);
+    axios.post('url',fd)
+      .then(res=>{
+        console.log(res);
+      });
+  }
+
+
+  render(){
+    return (
+      <>
+      <Stack direction="column" alignItems="left" spacing={1}>
+        <label htmlFor={this.value}>
+          <Input id={this.value} accept=".csv"   multiple type="file" onChange={this.fileSelectedHandler} />
+          
+          <ThemeProvider theme={theme}>
+          <Button color="neutral" variant="contained" component="span" size="large" onChange = {this.fileUploadHandler}>
+              {this.btnname}
+              
+            <IconButton style={{ color:'white' }} aria-label="upload picture" component="span">
+            <UploadFileIcon fontSize="large"/>
+            </IconButton>
+          </Button>
+          </ThemeProvider>
+        </label>
+       
         
+        
+      </Stack>
+     
+      <Stack direction= 'column' alignItems="center">
+      <label>
+          <ThemeProvider theme={theme}>
+          <Button color="neutral" variant="contained" component="span" size="small" onClick = {this.fileUploadHandler}>
+                           
+          <p> {this.state.FileName}</p>
+          </Button>
+          </ThemeProvider>
       </label>
-    </Stack>
-  );
+      </Stack>
+      
+      </>
+    );
+  }
+  
 }
