d8a1a019143c8fc9d523d223e086d090efcc5fa0
SashiniLiyanage <e17190@eng.pdn.ac.lk>
2021-09-26 15:49:35 +0530

    update student app - add feature to save video locally

--- Student_desktopapp/src/js/home.js
+++ Student_desktopapp/src/js/home.js
@@ -1,2 +1,10 @@
+document.getElementById('testmic').addEventListener('click', () => {
+    ipc.send("settings")
+})
+document.getElementById('testcam').addEventListener('click', () => {
+    ipc.send("settings")
+})
 
-
+document.getElementById('schedule').addEventListener('click', () => {
+    ipc.send("schedule")
+})
