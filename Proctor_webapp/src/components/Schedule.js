1c4f03dc05e1e21504a2fefe67493a328286b71e
isuridevindi <e17058@eng.pdn.ac.lk>
2021-09-27 21:41:15 +0530

    Add proctor's nav bar with routing

---
+++ Proctor_webapp/src/components/Schedule.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProctorAppBar from './ProctorAppBar'
+function Schedule() {
+    return (
+        <div style={{color:"black"}}>
+           <ProctorAppBar/>
+           Schedule
+        </div>
+    )
+}
+
+export default Schedule
