5640c2d54e2729cb1261f05386ab31b7a62ff2d1
savindu <e17369@eng.pdn.ac.lk>
2021-10-21 16:32:32 +0530

    fixed errors in the admin pp upload call

--- server/api/middleware/uploadProfPic.js
+++ server/api/middleware/uploadProfPic.js
@@ -1,17 +1,18 @@
 const crypto = require('crypto');
 const multer = require('multer');
+var path = require('path');
 
 
 // set storage engine to store admin/proctor/student profile pictures
 const storage = multer.diskStorage({
-    deestination: './public/profile_pictures',
+    destination: './public/profile_pictures',
     filename: function(req, file, cb){
       crypto.randomBytes(16, (err, buf) => {
         if(err) {
           console.log("Error occured trying to create crypto.randomBytes: " + err);
           return rejects(err);  // ERROR MIGHT HAPPEN HERE
         }
-        cb(null, buf.toString('hex') + '-' + Date.now() + path.extname(file.originalame));
+        cb(null, buf.toString('hex') + '-' + Date.now() + path.extname(file.originalname));
       });
     
     }
